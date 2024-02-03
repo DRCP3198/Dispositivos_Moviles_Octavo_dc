582aaacc4bb93c92bce6900555d6422f2102128a
Dillan Coloma <renatocoloma2010@gmail.com>
2024-02-02 23:42:02 -0500

    Integracion con Firebase y autenticación de usuarios

--- app/src/main/java/com/biometric/dc/MainActivity.kt
+++ app/src/main/java/com/biometric/dc/MainActivity.kt
@@ -14,11 +14,17 @@ import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
 import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
 import androidx.biometric.BiometricPrompt
 import androidx.core.content.ContextCompat
+import com.biometric.dc.databinding.ActivityMainBinding
 import com.biometric.dc.viewmodels.MainViewModel
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import java.util.concurrent.Executor
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
 
     //El Executor permite ejecutar procesos en segundo plano
     private lateinit var executor: Executor
@@ -34,9 +40,15 @@ class MainActivity : AppCompatActivity() {
 
     private val mainViewModel: MainViewModel by viewModels()
 
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
         initListener()
         initObservables()
         AuteticationVariables()
@@ -44,32 +56,47 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            //startActivity(Intent(this, MainActivity2::class.java))
+            //Se autentique con la huella digital
+            binding.editextUser.visibility = View.GONE
+            binding.editextPassword.visibility = View.GONE
+
+            binding.imgFinger.visibility = View.VISIBLE
+            binding.textInfo.text = getString(R.string.biometric_success)
+        } else {
+            binding.imgFinger.visibility = View.GONE
+            binding.textInfo.text = getString(R.string.no_user)
+        }
+    }
+
     private fun initListener() {
-
-        btnFinger = findViewById<ImageView>(R.id.imgFinger)
-        txtInfo = findViewById(R.id.textInfo)
-
-        btnFinger.setOnClickListener {
-
+        binding.imgFinger.setOnClickListener {
             biometricPrompt.authenticate(promptInfo)
         }
+        binding.btnSaveUsers.setOnClickListener {
+            createNewUsers(
+                binding.editextUser.text.toString(),
+                binding.editextPassword.text.toString()
+            )
+        }
+        binding.btnSignUser.setOnClickListener {
+            signInUsers(
+                binding.editextUser.text.toString(),
+                binding.editextPassword.text.toString()
+            )
+        }
+
     }
 
     private fun AuteticationVariables() {
         executor = ContextCompat.getMainExecutor(this)
-
         biometricPrompt = BiometricPrompt(this, executor,
             object : BiometricPrompt.AuthenticationCallback() {
-//                override fun onAuthenticationFailed() {
-//                    super.onAuthenticationFailed()
-//                    Log.d("MY_APP_TAG", "Authentication Failed")
-//                }
-//
-//                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
-//                    super.onAuthenticationError(errorCode, errString)
-//                    Log.d("MY_APP_TAG", "Authentication Error")
-//                }
-
                 override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
                     super.onAuthenticationSucceeded(result)
                     startActivity(Intent(this@MainActivity, MainActivity2::class.java))
@@ -84,24 +111,27 @@ class MainActivity : AppCompatActivity() {
             .build()
     }
 
-    private fun initObservables(){
+    private fun initObservables() {
 
-        mainViewModel.resultCheckBiometric.observe(this){code->
+        mainViewModel.resultCheckBiometric.observe(this) { code ->
 
             //Itero sobre lo que tengo en mi MainViewModel
-            when(code){
-                BiometricManager.BIOMETRIC_SUCCESS ->{
-                    btnFinger.visibility = View.VISIBLE
-                    txtInfo.text = getString(R.string.biometric_success)
+            when (code) {
+                BiometricManager.BIOMETRIC_SUCCESS -> {
+                    binding.imgFinger.visibility = View.VISIBLE
+                    binding.textInfo.text = getString(R.string.biometric_success)
                     Log.d("MY_APP_TAG", "App can authenticate using biometrics.")
                 }
-                BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE->{
-                    txtInfo.text = getString(R.string.biometric_no_hardware)
+
+                BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> {
+                    binding.textInfo.text = getString(R.string.biometric_no_hardware)
                     Log.e("MY_APP_TAG", "No biometric features available on this device.")
                 }
-                BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE ->{
+
+                BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> {
                     Log.e("MY_APP_TAG", "Biometric features are currently unavailable.")
                 }
+
                 BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
                     // Prompts the user to create credentials that your app accepts.
                     val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
@@ -115,6 +145,57 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
+    }
+
+    private fun createNewUsers(user: String, password: String) {
+        auth.createUserWithEmailAndPassword(
+            user,
+            password
+
+        )
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    Snackbar.make(
+                        this,
+                        binding.editextUser,
+                        "createWithEmail:success",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                    binding.editextUser.text.clear()
+                    binding.editextPassword.text.clear()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Snackbar.make(
+                        this,
+                        binding.editextUser,
+                        task.exception!!.message.toString(),
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                    Log.d("TAG", task.exception!!.stackTraceToString())
+                }
+            }
+    }
+
+    private fun signInUsers(email: String, password: String) {
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    startActivity(Intent(this, MainActivity2::class.java))
+                } else {
+                    Log.w("TAG", "signInWithEmail:failure", task.exception)
+                    Snackbar.make(
+                        this,
+                        binding.editextUser,
+                        "signInWithEmail:failure",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                }
+            }
     }
 
 
